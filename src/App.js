5f5552de8b6fb2aab7392f7d02078a6a3938808a
Bagher hosseini <Bagher.Hosseini@yh.nackademin.se>
2024-07-23 15:46:19 +0200

    feat: setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
-function App() {
+import * as features from './features';
+import * as pages from './pages';
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      children: [
+        {
+          element: <features.UnProtectedRouteLayout />,
+          children: [
+            { path: "/signIn/*", element: <pages.SignIn /> },
+            { path: "/signUp/*", element: <pages.SignUp /> },
+            { path: "/", element: <pages.Main /> },
+            { path: "/about", element: <pages.About /> },
+            { path: "/shop", element: <pages.Shop /> },
+            { path: "/cart", element: <pages.Cart /> },
+            { path: "/shop/product/:productId", element: <pages.Product /> },
+            { path: "*", element: <div style={{ background: "blue" }}>404</div> },
+          ]
+        },
+        {
+          element: <features.ProtectedRouteLayout />,
+          children: [
+            { path: "/afterSignIn", element: <div style={{ background: "red" }}>Protected Route</div> },
+          ]
+        },
+      ]
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <RouterProvider router={router} />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
+    </main>
   );
 }
-
-export default App;
